44079977b52791eadab1c62681aee1738a59fb8c
Ubuntu <ubuntu@localhost.localdomain>
2026-03-16 19:50:44 +0800

    add experiment&sample ui

--- lims_client/views/experiment_view.py
+++ lims_client/views/experiment_view.py
@@ -2,7 +2,7 @@
 from PySide6.QtWidgets import (
     QWidget, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox,
     QFormLayout, QLineEdit, QPushButton, QTableWidget,
-    QHeaderView, QAbstractItemView, QDateEdit, QTextEdit, QComboBox, QTableWidgetItem
+    QHeaderView, QAbstractItemView, QDateEdit, QTextEdit, QComboBox, QTableWidgetItem, QStackedWidget
 )
 from PySide6.QtCore import Qt, QDate
 
@@ -10,7 +10,67 @@ class ExperimentView(QWidget):
     """实验管理视图"""
     def __init__(self):
         super().__init__()
-        main_layout = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
+        self.stacked_widget = QStackedWidget()
+        self.main_layout.addWidget(self.stacked_widget)
+
+        self._setup_experiment_list_view()
+        self._setup_create_experiment_view()
+
+        # Set initial view to experiment list
+        self.stacked_widget.setCurrentWidget(self.experiment_list_container)
+
+    def _setup_experiment_list_view(self):
+        self.experiment_list_container = QWidget()
+        container_layout = QVBoxLayout(self.experiment_list_container)
+
+        # 2. 实验列表 (Experiment List)
+        experiment_list_group_box = QGroupBox("实验列表")
+        experiment_list_layout = QVBoxLayout(experiment_list_group_box)
+
+        header_layout = QHBoxLayout()
+        self.search_input = QLineEdit()
+        self.search_input.setPlaceholderText("搜索实验名称或负责人...")
+        header_layout.addWidget(self.search_input)
+
+        self.search_button = QPushButton("搜索")
+        header_layout.addWidget(self.search_button)
+
+        self.create_new_experiment_button = QPushButton("创建新实验")
+        self.create_new_experiment_button.clicked.connect(self._show_create_experiment_view)
+        header_layout.addWidget(self.create_new_experiment_button)
+
+        experiment_list_layout.addLayout(header_layout)
+
+        self.experiment_table = QTableWidget()
+        self.experiment_table.setColumnCount(5)
+        self.experiment_table.setHorizontalHeaderLabels(["实验名称", "状态", "开始日期", "结束日期", "负责人"])
+        self.experiment_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.experiment_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.experiment_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # Placeholder data
+        self.experiment_table.insertRow(0)
+        self.experiment_table.setItem(0, 0, QTableWidgetItem("项目A - 病毒分析"))
+        self.experiment_table.setItem(0, 1, QTableWidgetItem("进行中"))
+        self.experiment_table.setItem(0, 2, QTableWidgetItem("2026-03-01"))
+        self.experiment_table.setItem(0, 3, QTableWidgetItem("2026-04-01"))
+        self.experiment_table.setItem(0, 4, QTableWidgetItem("张三"))
+
+        self.experiment_table.insertRow(1)
+        self.experiment_table.setItem(1, 0, QTableWidgetItem("项目B - 细胞培养"))
+        self.experiment_table.setItem(1, 1, QTableWidgetItem("已完成"))
+        self.experiment_table.setItem(1, 2, QTableWidgetItem("2026-02-10"))
+        self.experiment_table.setItem(1, 3, QTableWidgetItem("2026-02-28"))
+        self.experiment_table.setItem(1, 4, QTableWidgetItem("李四"))
+
+        experiment_list_layout.addWidget(self.experiment_table)
+        container_layout.addWidget(experiment_list_group_box)
+        self.stacked_widget.addWidget(self.experiment_list_container)
+
+    def _setup_create_experiment_view(self):
+        self.create_experiment_container = QWidget()
+        container_layout = QVBoxLayout(self.create_experiment_container)
 
         # 1. 创建实验 (Create Experiment)
         create_experiment_group_box = QGroupBox("创建实验")
@@ -38,48 +98,20 @@ class ExperimentView(QWidget):
         self.experiment_status_combo.addItems(["草稿", "进行中", "已完成", "暂停"])
         create_experiment_layout.addRow("状态:", self.experiment_status_combo)
 
+        action_buttons_layout = QHBoxLayout()
         self.create_experiment_button = QPushButton("创建实验")
-        create_experiment_layout.addRow(self.create_experiment_button)
+        self.back_to_list_button_exp = QPushButton("返回列表")
+        self.back_to_list_button_exp.clicked.connect(self._show_experiment_list_view)
+        action_buttons_layout.addWidget(self.create_experiment_button)
+        action_buttons_layout.addWidget(self.back_to_list_button_exp)
+        create_experiment_layout.addRow(action_buttons_layout)
 
-        main_layout.addWidget(create_experiment_group_box)
+        container_layout.addWidget(create_experiment_group_box)
+        container_layout.addStretch(1)
+        self.stacked_widget.addWidget(self.create_experiment_container)
 
-        # 2. 实验列表 (Experiment List)
-        experiment_list_group_box = QGroupBox("实验列表")
-        experiment_list_layout = QVBoxLayout(experiment_list_group_box)
+    def _show_create_experiment_view(self):
+        self.stacked_widget.setCurrentWidget(self.create_experiment_container)
 
-        search_layout = QHBoxLayout()
-        self.search_input = QLineEdit()
-        self.search_input.setPlaceholderText("搜索实验名称或负责人...")
-        search_layout.addWidget(self.search_input)
-
-        self.search_button = QPushButton("搜索")
-        search_layout.addWidget(self.search_button)
-        experiment_list_layout.addLayout(search_layout)
-
-        self.experiment_table = QTableWidget()
-        self.experiment_table.setColumnCount(5)
-        self.experiment_table.setHorizontalHeaderLabels(["实验名称", "状态", "开始日期", "结束日期", "负责人"])
-        self.experiment_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.experiment_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.experiment_table.setSelectionBehavior(QAbstractItemView.SelectRows)
-
-        # Placeholder data
-        self.experiment_table.insertRow(0)
-        self.experiment_table.setItem(0, 0, QTableWidgetItem("项目A - 病毒分析"))
-        self.experiment_table.setItem(0, 1, QTableWidgetItem("进行中"))
-        self.experiment_table.setItem(0, 2, QTableWidgetItem("2026-03-01"))
-        self.experiment_table.setItem(0, 3, QTableWidgetItem("2026-04-01"))
-        self.experiment_table.setItem(0, 4, QTableWidgetItem("张三"))
-
-        self.experiment_table.insertRow(1)
-        self.experiment_table.setItem(1, 0, QTableWidgetItem("项目B - 细胞培养"))
-        self.experiment_table.setItem(1, 1, QTableWidgetItem("已完成"))
-        self.experiment_table.setItem(1, 2, QTableWidgetItem("2026-02-10"))
-        self.experiment_table.setItem(1, 3, QTableWidgetItem("2026-02-28"))
-        self.experiment_table.setItem(1, 4, QTableWidgetItem("李四"))
-
-
-        experiment_list_layout.addWidget(self.experiment_table)
-        main_layout.addWidget(experiment_list_group_box)
-
-        main_layout.addStretch(1)
+    def _show_experiment_list_view(self):
+        self.stacked_widget.setCurrentWidget(self.experiment_list_container)
--- lims_client/views/sample_view.py
+++ lims_client/views/sample_view.py
@@ -2,7 +2,7 @@
 from PySide6.QtWidgets import (
     QWidget, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox,
     QFormLayout, QLineEdit, QPushButton, QTableWidget,
-    QHeaderView, QAbstractItemView, QDateEdit, QTextEdit, QComboBox, QTableWidgetItem
+    QHeaderView, QAbstractItemView, QDateEdit, QTextEdit, QComboBox, QTableWidgetItem, QStackedWidget
 )
 from PySide6.QtCore import Qt, QDate
 
@@ -10,7 +10,69 @@ class SampleView(QWidget):
     """样本管理视图"""
     def __init__(self):
         super().__init__()
-        main_layout = QVBoxLayout(self)
+        self.main_layout = QVBoxLayout(self)
+        self.stacked_widget = QStackedWidget()
+        self.main_layout.addWidget(self.stacked_widget)
+
+        self._setup_sample_list_view()
+        self._setup_sample_registration_view()
+
+        # Set initial view to sample list
+        self.stacked_widget.setCurrentWidget(self.sample_list_container)
+
+    def _setup_sample_list_view(self):
+        self.sample_list_container = QWidget()
+        container_layout = QVBoxLayout(self.sample_list_container)
+
+        # 2. 样本列表 (Sample List)
+        sample_list_group_box = QGroupBox("样本列表")
+        sample_list_layout = QVBoxLayout(sample_list_group_box)
+
+        header_layout = QHBoxLayout()
+        self.search_sample_input = QLineEdit()
+        self.search_sample_input.setPlaceholderText("搜索样本ID、类型或来源...")
+        header_layout.addWidget(self.search_sample_input)
+
+        self.search_sample_button = QPushButton("搜索")
+        header_layout.addWidget(self.search_sample_button)
+
+        self.register_new_sample_button = QPushButton("登记新样本")
+        self.register_new_sample_button.clicked.connect(self._show_sample_registration_view)
+        header_layout.addWidget(self.register_new_sample_button)
+
+        sample_list_layout.addLayout(header_layout)
+
+        self.sample_table = QTableWidget()
+        self.sample_table.setColumnCount(6)
+        self.sample_table.setHorizontalHeaderLabels(["样本ID", "类型", "来源", "数量", "存储位置", "状态"])
+        self.sample_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.sample_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.sample_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # Placeholder data
+        self.sample_table.insertRow(0)
+        self.sample_table.setItem(0, 0, QTableWidgetItem("S001"))
+        self.sample_table.setItem(0, 1, QTableWidgetItem("血液"))
+        self.sample_table.setItem(0, 2, QTableWidgetItem("患者A"))
+        self.sample_table.setItem(0, 3, QTableWidgetItem("5ml"))
+        self.sample_table.setItem(0, 4, QTableWidgetItem("冰箱1, 架2"))
+        self.sample_table.setItem(0, 5, QTableWidgetItem("可用"))
+
+        self.sample_table.insertRow(1)
+        self.sample_table.setItem(1, 0, QTableWidgetItem("S002"))
+        self.sample_table.setItem(1, 1, QTableWidgetItem("组织"))
+        self.sample_table.setItem(1, 2, QTableWidgetItem("小鼠B"))
+        self.sample_table.setItem(1, 3, QTableWidgetItem("1g"))
+        self.sample_table.setItem(1, 4, QTableWidgetItem("液氮罐3"))
+        self.sample_table.setItem(1, 5, QTableWidgetItem("已使用"))
+
+        sample_list_layout.addWidget(self.sample_table)
+        container_layout.addWidget(sample_list_group_box)
+        self.stacked_widget.addWidget(self.sample_list_container)
+
+    def _setup_sample_registration_view(self):
+        self.sample_registration_container = QWidget()
+        container_layout = QVBoxLayout(self.sample_registration_container)
 
         # 1. 样本登记 (Sample Registration)
         sample_registration_group_box = QGroupBox("样本登记")
@@ -43,49 +105,20 @@ class SampleView(QWidget):
         self.sample_remarks_input.setPlaceholderText("请输入备注...")
         sample_registration_layout.addRow("备注:", self.sample_remarks_input)
 
+        action_buttons_layout = QHBoxLayout()
         self.register_sample_button = QPushButton("登记样本")
-        sample_registration_layout.addRow(self.register_sample_button)
+        self.back_to_list_button_sample = QPushButton("返回列表")
+        self.back_to_list_button_sample.clicked.connect(self._show_sample_list_view)
+        action_buttons_layout.addWidget(self.register_sample_button)
+        action_buttons_layout.addWidget(self.back_to_list_button_sample)
+        sample_registration_layout.addRow(action_buttons_layout)
 
-        main_layout.addWidget(sample_registration_group_box)
+        container_layout.addWidget(sample_registration_group_box)
+        container_layout.addStretch(1)
+        self.stacked_widget.addWidget(self.sample_registration_container)
 
-        # 2. 样本列表 (Sample List)
-        sample_list_group_box = QGroupBox("样本列表")
-        sample_list_layout = QVBoxLayout(sample_list_group_box)
+    def _show_sample_registration_view(self):
+        self.stacked_widget.setCurrentWidget(self.sample_registration_container)
 
-        search_layout = QHBoxLayout()
-        self.search_sample_input = QLineEdit()
-        self.search_sample_input.setPlaceholderText("搜索样本ID、类型或来源...")
-        search_layout.addWidget(self.search_sample_input)
-
-        self.search_sample_button = QPushButton("搜索")
-        search_layout.addWidget(self.search_sample_button)
-        sample_list_layout.addLayout(search_layout)
-
-        self.sample_table = QTableWidget()
-        self.sample_table.setColumnCount(6)
-        self.sample_table.setHorizontalHeaderLabels(["样本ID", "类型", "来源", "数量", "存储位置", "状态"])
-        self.sample_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.sample_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.sample_table.setSelectionBehavior(QAbstractItemView.SelectRows)
-
-        # Placeholder data
-        self.sample_table.insertRow(0)
-        self.sample_table.setItem(0, 0, QTableWidgetItem("S001"))
-        self.sample_table.setItem(0, 1, QTableWidgetItem("血液"))
-        self.sample_table.setItem(0, 2, QTableWidgetItem("患者A"))
-        self.sample_table.setItem(0, 3, QTableWidgetItem("5ml"))
-        self.sample_table.setItem(0, 4, QTableWidgetItem("冰箱1, 架2"))
-        self.sample_table.setItem(0, 5, QTableWidgetItem("可用"))
-
-        self.sample_table.insertRow(1)
-        self.sample_table.setItem(1, 0, QTableWidgetItem("S002"))
-        self.sample_table.setItem(1, 1, QTableWidgetItem("组织"))
-        self.sample_table.setItem(1, 2, QTableWidgetItem("小鼠B"))
-        self.sample_table.setItem(1, 3, QTableWidgetItem("1g"))
-        self.sample_table.setItem(1, 4, QTableWidgetItem("液氮罐3"))
-        self.sample_table.setItem(1, 5, QTableWidgetItem("已使用"))
-
-        sample_list_layout.addWidget(self.sample_table)
-        main_layout.addWidget(sample_list_group_box)
-
-        main_layout.addStretch(1)
+    def _show_sample_list_view(self):
+        self.stacked_widget.setCurrentWidget(self.sample_list_container)
